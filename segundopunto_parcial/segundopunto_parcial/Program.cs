dfbaf8a67b3998855d8a47be9ddef579e44365a9
brayan usme <brayanelgamer2@gmail.com>
2024-09-13 22:24:06 -0500

    actuali1.2

--- segundopunto_parcial/segundopunto_parcial/Program.cs
+++ segundopunto_parcial/segundopunto_parcial/Program.cs
@@ -1,8 +1,17 @@
-﻿public class Program
+﻿using System;
+
+public class Program
 {
     private static void Main(string[] args)
     {
-       
+        /*Escriba un programa que pida los coeficientes de una ecuación de primer grado (a x + b = 0) y muestre la solución.
+        Una ecuación de primer grado puede no tener solución, tener una solución única, o que todos los números sean solución. La fórmula de las soluciones es x = -b / a
+        */
+
+        // pedir variables y inicializar de una vez a fuegooooo
+
+        Console.WriteLine("Introduce el valor de a (el numerito de x): ");
+        double a = Convert.ToDouble(Console.ReadLine());
 
 
 
